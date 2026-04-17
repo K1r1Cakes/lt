b7316b611a852fc492e7156faaf20d48d879893d
morisscofield <msmukwayi@gmail.com>
2026-04-17 15:07:50 +0200

    Catch me if you can

--- test_getCols.py
+++ test_getCols.py
@@ -4,7 +4,7 @@ from io import StringIO
 def test_getCols():
     file = StringIO("Course, Student Number, Mark, Comment")
     
-    num_col, mark_col = getCols(file)
+    num_col, mark_col = getCols(File)
 
     assert num_col == 1
     assert mark_col == 2
